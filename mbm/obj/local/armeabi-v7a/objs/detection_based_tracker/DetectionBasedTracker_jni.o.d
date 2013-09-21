518dd6ee515808ba19608c6bc40c581ad34925f2
Adam Pfenninberger <adam@pfenninberger.hu>
2013-09-21 16:24:21 +0200

    Initial commit

---
+++ mbm/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/jni.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/assert.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/string.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/strings.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/memory.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/math.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/limits.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/jni.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/string.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/math.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/home/adam/apps/adt-bundle-linux-x86_64-20130219/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/log.h:
